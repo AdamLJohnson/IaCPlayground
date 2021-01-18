e4eff1e8fb34a75929575f1fa8afe59d21c49fca
Adam Johnson <columbus@adamlj.com>
2021-01-17 18:03:13 -0800

    Removed Table

--- DynamoDb/main.tf
+++ DynamoDb/main.tf
@@ -9,15 +9,3 @@ resource "aws_dynamodb_table" "userinfo-table" {
     type = "N"
   }
 }
-
-resource "aws_dynamodb_table" "testatlantis" {
-  name           = "testatlantis"
-  read_capacity  = 5
-  write_capacity = 5
-  hash_key       = "UserId"
-
-  attribute {
-    name = "UserId"
-    type = "N"
-  }
-}
